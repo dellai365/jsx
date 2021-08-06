a0625d1a98745463c7195b2c974173279de57508
dellai365 <77741324+dellai365@users.noreply.github.com>
2021-08-06 09:51:47 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Form } from 'react-bootstrap';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div style={{border:"solid 1px black",maxwidth:'100vw'}}>
+
+          <h1 className="title red">Dellai Mohamed Ali</h1>
+
+          <br />
+
+          <img src="/imageInSrc.jpg" />
+
+          <br />
+
+          <img src="/imageInPublic.jpg" />
+
+      </div>
+
+      <video width="320" height="240" controls>
+
+        <source src="myVideo.mp4" type="video/mp4" />
+
+      </video>
+    </div>
+    );
+}
+export default App;
